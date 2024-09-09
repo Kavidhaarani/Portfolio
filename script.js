c611bddf8a43579560d77be18a29dede9c6d0da9
Kavidhaarani <skavidhaarani@gmail.com>
2024-09-09 19:35:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+function mysubmit(){
+    alert("Form Submitted Successfully!!!");
+}
+
+
